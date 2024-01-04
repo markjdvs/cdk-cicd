40a245e45526e4215c9e389a404905840067f4bb
Mark Davis <markjdvs@gmail.com>
2024-01-04 23:24:13 +0000

    feat: pipeline init

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'TestPipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('markjdvs/cdk-cicd', 'cicd-pratice'),
+        commands: [
+          'cd cdk-cicd',
+          'npm ci',
+          'npx cdk synth',
+        ],
+        primaryOutputDirectory: 'cdk-cicd/cdk.out', // not needed here but useful
+      }),
+    });
   }
 }
