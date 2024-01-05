e9a1bc1117f351839ed445e88ea168172d14654d
Mark Davis <markjdvs@gmail.com>
2024-01-05 00:45:43 +0000

    feat: unit test pipeline step

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,5 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { PipelineStage } from './PipelineStage';
 
@@ -21,5 +21,12 @@ export class CdkCicdStack extends cdk.Stack {
     const testStage = pipeline.addStage(new PipelineStage(this, 'TestPipelineStage', {
       stageName: 'test',
     }));
+
+    testStage.addPre(new CodeBuildStep('unit-tests', {
+      commands: [
+        'npm ci',
+        'npm test',
+      ]
+    }))
   }
 }
